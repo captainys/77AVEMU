eece7a49ad046ac4d521ebcfe9045d11a17c9b9f
Soji Yamakawa <soji@andrew.cmu.edu>
2022-07-02 23:44:37 -0400

    Better test for DAA.

--- src/tests/adc_daa.cpp
+++ src/tests/adc_daa.cpp
@@ -33,7 +33,7 @@ public:
 };
 
 // From Polar Star III.  By adding $99999999 to BCD, it can essentially subtract 1.  Same as adding $FFFF to subtract one.
-bool DAA9999(int bcd,bool verbose)
+bool DAA(int bcd1,int bcd2,bool verbose)
 {
 	TestVM vm;
 	static MC6809 cpu(&vm);
@@ -49,13 +49,20 @@ bool DAA9999(int bcd,bool verbose)
 	}
 
 	unsigned int bcdToHex;
-	bcdToHex=((bcd/1000)%10)*0x1000+
-	         ((bcd/100)%10) *0x100+
-	         ((bcd/10)%10)  *0x10+
-	          (bcd%10);
+	bcdToHex=((bcd1/1000)%10)*0x1000+
+	         ((bcd1/100)%10) *0x100+
+	         ((bcd1/10)%10)  *0x10+
+	          (bcd1%10);
 
 	mem.RAM[0x2001]=(bcdToHex>>8);
 	mem.RAM[0x2002]=(bcdToHex&0xFF);
+
+	bcdToHex=((bcd2/1000)%10)*0x1000+
+	         ((bcd2/100)%10) *0x100+
+	         ((bcd2/10)%10)  *0x10+
+	          (bcd2%10);
+	mem.RAM[0x2006]=(bcdToHex&0xFF);
+	mem.RAM[0x200B]=(bcdToHex>>8);
 
 	cpu.state.PC=0x2000;
 	mem.RAM[0]=0;
@@ -88,11 +95,11 @@ bool DAA9999(int bcd,bool verbose)
 		cpu.RunOneInstruction(mem);
 	}
 
-	--bcd;
-	bcdToHex=((bcd/1000)%10)*0x1000+
-	         ((bcd/100)%10) *0x100+
-	         ((bcd/10)%10)  *0x10+
-	          (bcd%10);
+	auto sum=(bcd1+bcd2)%10000;
+	bcdToHex=((sum/1000)%10)*0x1000+
+	         ((sum/100)%10) *0x100+
+	         ((sum/10)%10)  *0x10+
+	          (sum%10);
 	unsigned int D=cpu.state.A();
 	D<<=8;
 	D|=cpu.state.B();
@@ -107,13 +114,22 @@ bool DAA9999(int bcd,bool verbose)
 
 int main(void)
 {
-	DAA9999(2,true);
 	for(int i=1; i<9999; ++i)
 	{
-		if(0!=DAA9999(i,false))
+		if(0!=DAA(i,9999,false))
 		{
 			printf("Failed at %d\n",i);
 			return 1;
+		}
+	}
+	for(int i=0; i<=1000; ++i)
+	{
+		for(int j=0; j<=1000; ++j)
+		{
+			if(0!=DAA(i,j,false))
+			{
+				printf("Failed at %d+%d\n",i,j);
+			}
 		}
 	}
 	std::cout << "All Passes." << std::endl;
