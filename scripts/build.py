eab4042fe97ab701ab7b1ddb4b2711b51d667d8c
Soji Yamakawa <soji@andrew.cmu.edu>
2022-07-10 17:21:34 -0400

    Added scripts.

---
+++ scripts/build.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import shutil
+import sys
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+BUILDDIR=os.path.join(THISDIR,"..","build")
+SRCDIR=os.path.join(THISDIR,"..","src")
+
+
+
+def Run():
+	cwd=os.getcwd()
+
+	os.chdir(BUILDDIR)
+
+	cmd=[
+		"cmake",
+		"--build",
+		".",
+		"--config",
+		"Release",
+		"--parallel",
+	]
+
+	proc=subprocess.Popen(cmd)
+	proc.communicate();
+
+	print(cmd)
+
+	os.chdir(cwd)
+
+	if proc.returncode!=0:
+		print("Build Error!")
+		quit()
+
+
+
+if __name__=="__main__":
+	Run()
+
+
---
+++ scripts/run7.py
@@ -0,0 +1,59 @@
+import os
+import subprocess
+import shutil
+import sys
+import sys
+
+import build
+
+MACHINETYPE="7"
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+MUTSUDIR=os.path.join(THISDIR,"..")
+
+BUILDDIR=os.path.join(MUTSUDIR,"build")
+SRCDIR=os.path.join(MUTSUDIR,"src")
+ROMDIR=os.path.join(MUTSUDIR,"..","77AVTEST","ROMS",MACHINETYPE)
+DISKDIR=os.path.join(MUTSUDIR,"..","77AVTEST","DISK")
+TAPEDIR=os.path.join(MUTSUDIR,"..","77AVTEST","TAPE")
+
+
+
+def ExeExtension():
+	if sys.platform.startswith('win'):
+		return ".exe"
+	else:
+		return ""
+
+
+
+def MutsuExe():
+	fName=os.path.join(MUTSUDIR,"build","main_cui","Mutsu_CUI"+ExeExtension())
+	if os.path.isfile(fName):
+		return fName
+	fName=os.path.join(MUTSUDIR,"build","main_cui","Release","Mutsu_CUI"+ExeExtension())
+	if os.path.isfile(fName):
+		return fName
+	fName=os.path.join(MUTSUDIR,"build","main_cui","Mutsu_CUI.app","Contents","MacOS","Mutsu_CUI"+ExeExtension())
+	if os.path.isfile(fName):
+		return fName
+	throw
+
+
+
+def Run(argv):
+	subprocess.Popen([
+		MutsuExe(),
+		ROMDIR,
+		#"-SYM",
+		#os.path.join(MUTSUDIR,"symtables","RUN"+MACHINETYPE+".txt"),
+		#"-DEBUG",
+		#"-PAUSE",
+	]+argv).wait()
+
+
+
+if __name__=="__main__":
+	build.Run()
+	Run(sys.argv[1:])
