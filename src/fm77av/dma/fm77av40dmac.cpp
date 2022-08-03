f163963220c73308bc2d3e113838adad735c4927
Soji Yamakawa <soji@andrew.cmu.edu>
2022-08-02 20:39:37 -0400

    Working on DMA

--- src/fm77av/dma/fm77av40dmac.cpp
+++ src/fm77av/dma/fm77av40dmac.cpp
@@ -9,12 +9,67 @@ FM77AV40DMAC::FM77AV40DMAC(VMBase *vmBase) : Device(vmBase)
 
 void FM77AV40DMAC::Reset(void)
 {
-	state.enabled=false;
 	for(auto &d: state.regs)
 	{
 		d=0;
 	}
 }
+
+bool FM77AV40DMAC::TxRQ(void) const
+{
+	return (0!=(state.regs[0x14]&1));
+}
+uint16_t FM77AV40DMAC::Address(void) const
+{
+	uint16_t data=state.regs[0];
+	data<<=8;
+	data|=state.regs[1];
+	return data;
+}
+uint16_t FM77AV40DMAC::NumBytes(void) const
+{
+	uint16_t data=state.regs[2];
+	data<<=8;
+	data|=state.regs[3];
+	return data;
+}
+bool FM77AV40DMAC::AddressGoUp(void) const
+{
+	return 0!=(state.regs[0x10]&8);
+}
+bool FM77AV40DMAC::FDCtoMEM(void) const
+{
+	return 0!=(state.regs[0x10]&1);
+}
+void FM77AV40DMAC::SetDMAEnd(bool ended)
+{
+	state.regs[0x10]&=0x7F;
+	if(true==ended)
+	{
+		state.regs[0x10]|=0x80;
+	}
+}
+void FM77AV40DMAC::SetBusy(bool busy)
+{
+	state.regs[0x10]&=0xBF;
+	if(true==busy)
+	{
+		state.regs[0x10]|=0x40;
+	}
+}
+bool FM77AV40DMAC::IRQEnabled(void) const
+{
+	return 0!=(state.regs[0x15]&1);
+}
+void FM77AV40DMAC::SetIRQ(bool IRQ)
+{
+	state.regs[0x15]&0x7F;
+	if(true==IRQ)
+	{
+		state.regs[0x15]|=0x80;
+	}
+}
+
 void FM77AV40DMAC::WriteFD98(uint8_t data)
 {
 	state.addrLatch=(data&0x1F);
--- src/fm77av/dma/fm77av40dmac.h
+++ src/fm77av/dma/fm77av40dmac.h
@@ -22,11 +22,24 @@ public:
 	public:
 		uint8_t addrLatch=0;
 		uint8_t regs[NUM_REGS];
-		bool enabled=false;
 	};
 	State state;
 
 	void Reset(void);
+
+	/*! If true, DMA enabled.
+	*/
+	bool TxRQ(void) const;
+
+	uint16_t Address(void) const;
+	uint16_t NumBytes(void) const;
+	bool AddressGoUp(void) const;
+	bool FDCtoMEM(void) const;
+	void SetDMAEnd(bool ended);
+	void SetBusy(bool busy);
+
+	bool IRQEnabled(void) const;
+	void SetIRQ(bool IRQ);
 
 	void WriteFD98(uint8_t data);
 	void WriteFD99(uint8_t data);
